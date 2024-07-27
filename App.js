717fbf8268f9c8789018ca4877ce999f51b4d7c3
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-07-27 11:35:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World of Students</Text>
+      <Text>Hey Wassup ?</Text>
       <StatusBar style="auto" />
     </View>
   );
